739779ee2d77c5563d04485221e8b382040e0d97
vichyssoise <v@vichyssoise.io>
2018-02-05 23:50:59 +0900

    Add Nullable tests

---
+++ tests/decorators/Nullable.test.ts
@@ -0,0 +1,13 @@
+import { Nullable } from '../../src'
+import { getFields } from '../../src/services'
+
+describe('@Nullable', () => {
+  it('sets nullable a field', () => {
+    class A {
+      @Nullable a: any
+    }
+
+    const nullable = getFields(A.prototype)['a'].nullable
+    expect(nullable).toBeTruthy()
+  })
+})
